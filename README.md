79839f5e0437ebd84496e5f4dccbb2563635d36e
Bilal Ahmad <bilal.codemaster@gmail.com>
2024-10-01 12:15:37 +0500

    sample backend

--- README.md
+++
@@ -1,1 +0,0 @@
-# VentureBridge-Backend
---
+++ app.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const PORT=process.env.PORT
+const express = require("express");
+const app = express();
+app.listen(PORT, () => {
+  console.log(`app is running PORT: ${PORT}`);
+});
